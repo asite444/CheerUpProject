d5b93353794514934be7197377088799652561d3
lumen9078 <lumen9078@gmail.com>
2025-02-10 12:27:24 +0900

    함수명을 보안에서 보완으로 변경,  report_security -> report_improvement

--- app/controllers/home_controller.py
+++ app/controllers/home_controller.py
@@ -41,7 +41,7 @@ async def process_user_data(user_data: UserInputData):
 
     report_top5 = sql.analyze_stack_top5(user_data)
     report_user_tech ="현재 임시 차단"#gpt.analyze_user_tech(user_data)
-    report_security = gpt.analyze_improvement(user_data)
+    report_improvement = gpt.analyze_improvement(user_data)
     report_conclusion = gpt.analyze_conclusion(user_data)
 
 
@@ -55,7 +55,7 @@ async def process_user_data(user_data: UserInputData):
     return JSONResponse(content={
         "report_top5": report_top5,
         "report_user_tech": report_user_tech,
-        "report_security": report_security,
+        "report_improvement": report_improvement,
         "report_conclusion": report_conclusion,
         "report_graph_career":report_graph_career,
         "report_graph_degree":report_graph_degree,
